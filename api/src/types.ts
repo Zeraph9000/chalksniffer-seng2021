def33052edb35657347e009a3c3c98b6e9479ff7
Parham Sepasgozar <z5690364@ad.unsw.edu.au>
2026-03-06 14:17:38 +1100

    Added types

---
+++ api/src/types.ts
@@ -0,0 +1,155 @@
+type Period = {
+  startDate?: string;
+  endDate?: string;
+};
+
+type DocumentReference = {
+  id: string;
+  documentType?: string;
+};
+
+type Address = {
+  streetName: string;
+  additionalStreetName?: string;
+  buildingNumber?: string;
+  cityName: string;
+  postalZone: string;
+  countrySubentity?: string;
+  country: string;
+};
+
+type Contact = {
+  name?: string;
+  telephone?: string;
+  email?: string;
+};
+
+type Party = {
+  partyName: string;
+  partyIdentification?: string;
+  postalAddress: Address;
+  contact?: Contact;
+};
+
+type CustomerParty = {
+  party: Party;
+};
+
+type SupplierParty = {
+  party: Party;
+};
+
+type TaxCategory = {
+  percent?: number;
+  taxScheme: string;
+};
+
+type TaxSubtotal = {
+  taxableAmount: number;
+  taxAmount: number;
+  taxCategory: TaxCategory;
+};
+
+type TaxTotal = {
+  taxAmount: number;
+  currencyID: string;
+  taxSubtotal?: TaxSubtotal[];
+};
+
+type MonetaryTotal = {
+  lineExtensionAmount?: number;
+  taxExclusiveAmount?: number;
+  taxInclusiveAmount?: number;
+  allowanceTotalAmount?: number;
+  chargeTotalAmount?: number;
+  payableAmount?: number;
+};
+
+type AllowanceCharge = {
+  chargeIndicator: boolean;
+  allowanceChargeReasonCode?: string;
+  allowanceChargeReason?: string;
+  amount: number;
+  currencyID: string;
+};
+
+type PaymentMeans = {
+  paymentMeansCode: string;
+  paymentDueDate?: string;
+  paymentID?: string;
+};
+
+type PaymentTerms = {
+  note?: string;
+};
+
+type Delivery = {
+  deliveryAddress?: Address;
+  requestedDeliveryPeriod?: Period;
+};
+
+type DeliveryTerms = {
+  specialTerms?: string;
+};
+
+type Price = {
+  priceAmount: number;
+  currencyID: string;
+  baseQuantity?: number;
+};
+
+type Item = {
+  name: string;
+  description?: string;
+  buyersItemIdentification?: string;
+  sellersItemIdentification?: string;
+  standardItemIdentification?: string;
+  classifiedTaxCategory?: TaxCategory;
+};
+
+type LineItem = {
+  id: string;
+  quantity: number;
+  unitCode?: string;
+  lineExtensionAmount?: number;
+  price: Price;
+  item: Item;
+  delivery?: Delivery;
+};
+
+type OrderLine = {
+  lineItem: LineItem;
+};
+
+type Order = {
+  id: string;
+  salesOrderId?: string;
+  issueDate: string;
+  issueTime?: string;
+  orderTypeCode?: string;
+  note?: string;
+  documentCurrencyCode: string;
+  pricingCurrencyCode?: string;
+  taxCurrencyCode?: string;
+  customerReference?: string;
+  accountingCostCode?: string;
+  validityPeriod?: Period;
+  quotationDocumentReference?: DocumentReference;
+  orderDocumentReference?: DocumentReference;
+  originatorDocumentReference?: DocumentReference;
+  additionalDocumentReference?: DocumentReference[];
+  buyerCustomerParty: CustomerParty;
+  sellerSupplierParty: SupplierParty;
+  originatorCustomerParty?: CustomerParty;
+  delivery?: Delivery;
+  deliveryTerms?: DeliveryTerms;
+  paymentMeans?: PaymentMeans;
+  paymentTerms?: PaymentTerms;
+  allowanceCharge?: AllowanceCharge[];
+  taxTotal?: TaxTotal;
+  anticipatedMonetaryTotal?: MonetaryTotal;
+  orderLines: OrderLine[];
+  createdAt?: string;
+  updatedAt?: string;
+  xmlUrl?: string;
+};
