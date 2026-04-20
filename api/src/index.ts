4244c4736c796759dc3292a5ddc30410d8c3cfd1
Andrew Davindra Hans <andrew@student.unsw.edu.au>
2026-04-20 23:27:46 +1000

    fixing deployment

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,9 +3,12 @@ import { connectDB } from './db';
 import app from './app';
 dotenv.config();
 
-const PORT = process.env.PORT ?? 3000;
-
 connectDB(process.env.MONGODB_URI ?? 'mongodb://localhost:27017/chalksniffer')
   .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    if (!process.env.VERCEL) {
+      const PORT = process.env.PORT ?? 3000;
+      app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    }
   });
+
+export default app;
